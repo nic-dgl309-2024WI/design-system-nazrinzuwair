d4c3ce1478be2d5dd1434cc23d3df543ca589bef
nazrinzuwair <naz4rinaz@gmail.com>
2024-02-04 18:55:51 -0800

    Updated mobile navigation toggle to change icon from hamburger to cross on open

--- design-system.js
+++ design-system.js
@@ -1,4 +1,8 @@
 function showNav() {
     var element = document.getElementById("nav-items");
+    var icon = document.getElementById("toggle-icon");
+
     element.classList.toggle("c-mobile-navbar__items");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-times");
 }
