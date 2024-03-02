acfd7e558dbc95407d5dfb876f6be2b8ca853c31
nazrinzuwair <naz4rinaz@gmail.com>
2024-03-02 13:03:31 -0800

    Updates to improve responsiveness of the side navigation.
    Function to toggle visibility of the desktop navigation menu and change the toggle icon between bars and times.

--- design-system.js
+++ design-system.js
@@ -1,7 +1,27 @@
-
 function showNav() {
     var navItems = document.getElementById("nav-items");
-    var naviItems = document.getElementById("navi-items");
+    var icon = document.getElementById("toggle-icon");
+
+    navItems.classList.toggle("c-desktop-navbar__items");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-times");
+}
+
+function showiNav() {
+    var navitems = document.getElementById("navi-items");
+    var icon = document.getElementById("toggle-icoon");
+    var sidebar = document.querySelector("aside");
+
+    navitems.classList.toggle("c-desktop-navbar__items");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-times");
+    sidebar.classList.toggle("show-sidebar");
+}
+
+
+function showMobileNav() {
+    var navItems = document.getElementById("mobile-nav-items");
+    var naviItems = document.getElementById("mobile-navig-items");
     var icon = document.getElementById("toggle-icon");
 
     navItems.classList.toggle("c-mobile-navbar__items");
@@ -9,3 +29,5 @@ function showNav() {
     icon.classList.toggle("fa-bars");
     icon.classList.toggle("fa-times");
 }
+
+
