624a3a91c1e17c5edebe4414f40226daa5e58cc4
nazrinzuwair <naz4rinaz@gmail.com>
2024-03-31 14:12:24 -0700

    Add navigation in mobile view

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // Toggles the visibility of navigation items.
 function showNav() {
     // Get the navigation items container element by its ID
-    var element = document.getElementById("header__c-navbar-items");
+    var element = document.getElementById("c-navbar-items");
     // Toggle the "show-items" class to control visibility
     element.classList.toggle("show-items");
 }
